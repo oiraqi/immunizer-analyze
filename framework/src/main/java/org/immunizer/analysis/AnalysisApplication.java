4f1f6eaf20509bc63d05a8a8f9e6c0308acdb785
Omar Iraqi <o.iraqi@gmail.com>
2020-01-07 17:39:06 +0100

    added AnalysisApplication

---
+++ framework/src/main/java/org/immunizer/analysis/AnalysisApplication.java
@@ -0,0 +1,27 @@
+package org.immunizer.analysis;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+
+import java.time.Duration;
+
+public class AnalysisApplication {
+
+    public static void main(String[] args) {
+        
+        FeatureRecordConsumer consumer = new FeatureRecordConsumer();
+
+        try {            
+            while (true) {
+                ConsumerRecords<String, FeatureRecord> records = consumer.poll(Duration.ofSeconds(5));
+                for (ConsumerRecord<String, FeatureRecord> record : records){
+                    System.out.printf("offset = %d, key = %s, value = %s%n",
+                        record.offset(), record.key(), record.value());
+                }
+            }
+        } finally {
+            consumer.close();
+        }
+    }
+
+}
